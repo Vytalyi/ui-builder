e06808610397bd85dedef1a5ed2700e4d169b051
Vitalii Denysenko <vitalii_denysenko@emap.com>
2014-11-28 13:26:32 +0200

    Refactored node app file due to "app crashes" issue on heroku

--- app.js
+++ app.js
@@ -1,33 +1,37 @@
-var express = require("express"),
-	routes = require("./routes"),
+﻿﻿var express = require('express'),
+	routes = require('./routes'),
+	http = require('http'),
+	path = require('path'),
+    favicon = require('serve-favicon'),
     bodyParser = require('body-parser'),
-    methodOverride = require('method-override'),
-    app = express();
+    cookieParser = require('cookie-parser'),
+    session = require('cookie-session'),
+    errorhandler = require('errorhandler');
 
-// basic routes
-routes.init(app);
 
+var app = express();
 app.set('port', process.env.PORT || 5000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
-app.use(bodyParser());
-app.use(methodOverride());
+app.use(favicon(__dirname + '/public/img/favicon.ico'));
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.set('trust proxy', 1);
+app.use(session({
+    keys: ['key1', 'key2']
+}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// 500 handle
-app.use(function(err, req, res, next) {
-    res.status(500);
-    res.render('500');
+
+routes.init(app);
+
+
+http.createServer(app).listen(app.get('port'), function () {
+	console.log("Express server listening on port " + app.get('port'));
 });
 
-// 404 handle
-app.get('*', function(req, res){
-    res.status(404);
-    res.render('404');
-});
 
-var server = app.listen(3000, function () {
-    var host = server.address().address,
-        port = server.address().port;
-
-    console.log('App is listening at http://%s:%s', host, port)
-});
+if (process.env.NODE_ENV === 'development') {
+    app.use(errorhandler());
+}
