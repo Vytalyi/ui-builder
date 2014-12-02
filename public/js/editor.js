68fbbc9ff2a9da60ddac1c094ec88b6d48fd0d33
Vitalii Denysenko <vitalii_denysenko@emap.com>
2014-12-02 17:00:34 +0200

    Injecting style with dashed borders on hover as a part of editor

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,5 +1,32 @@
 ;(function() {
+    var h = getAPI();
+    h.attachStyle("*:hover { border: 1px dashed #000; }");
 
-    alert("Editor has been injected successfully");
+
+    /* API */
+    /* **************** */
+    function getAPI() {
+        var API = function() {};
+
+        API.prototype.bind = function(eventName, eventHandler) {
+            el.addEventListener(eventName, eventHandler);
+        };
+
+        API.prototype.attachStyle =  function(css) {
+            var head = document.head || document.getElementsByTagName('head')[0],
+                style = document.createElement('style');
+
+            style.type = 'text/css';
+            if (style.styleSheet){
+                style.styleSheet.cssText = css;
+            } else {
+                style.appendChild(document.createTextNode(css));
+            }
+
+            head.appendChild(style);
+        };
+
+        return ( new API() );
+    }
 
 })();
--- routes/editor.js
+++ routes/editor.js
@@ -1,11 +1,17 @@
 module.exports = {
 
     init: function (app) {
-        app.get('/editor-api/inject/', this.redirectToInjectionScript);
+        app.get('/editor-api/inject/', this.allowCrossDomain, this.redirectToInjectionScript);
     },
 
     redirectToInjectionScript: function (req, res) {
-        res.redirect('/js/editor.js');
+        res.redirect('/js/editor.js?nocache=' + (new Date().getTime()));
+    },
+
+    allowCrossDomain: function(req, res, next) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        next();
     }
 
 };
