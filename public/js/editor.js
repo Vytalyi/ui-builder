435f525cc8a6a56f3b0d253eb6c074e31bb71255
Vitalii Denysenko <vitalii_denysenko@emap.com>
2014-12-04 17:35:42 +0200

    Added multicolor visualization of currently hovered element on page

--- public/js/editor.js
+++ public/js/editor.js
@@ -1,6 +1,33 @@
 ;(function() {
-    var h = getAPI();
-    h.attachStyle("*:hover { border: 1px dashed #000; }");
+    var api = getAPI();
+
+    (function() {
+
+        //api.attachStyle(".visualization:hover { border: 1px dashed #000 !important; }");
+        api.attachStyle("" +
+            ".visualization:hover { background: grey !important; }" +
+            ".visualization .visualization:hover { background: yellow !important; }" +
+            "");
+
+        var elements = document.querySelectorAll('body *');
+        for (var i in elements) {
+        	if (elements[i].style && elements.hasOwnProperty(i)) {
+        		var inn = elements[i], outt = inn.parentNode;
+
+                api.bind(outt, "mouseover", function(e) {
+                    e.stopPropagation();
+                });
+                api.bind(inn, "mouseout", function(e) {
+                    this.className = this.className.replace( /(?:^|\s)visualization(?!\S)/g , '' );
+                });
+                api.bind(inn, "mouseover", function(e) {
+                    this.className += " visualization";
+                });
+
+        	}
+        }
+
+    })();
 
 
     /* API */
@@ -8,8 +35,8 @@
     function getAPI() {
         var API = function() {};
 
-        API.prototype.bind = function(eventName, eventHandler) {
-            el.addEventListener(eventName, eventHandler);
+        API.prototype.bind = function(el, eventName, eventHandler) {
+            el.addEventListener(eventName, eventHandler, false);
         };
 
         API.prototype.attachStyle =  function(css) {
